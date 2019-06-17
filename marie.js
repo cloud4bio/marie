75cbcd680693dcbabcc96f273ace19878e624a43
Jonas Almeida <jalmeida@mathbiol.org>
2019-06-17 16:01:41 -0400

    startin things up

--- marie.js
+++ marie.js
@@ -1,1 +1,27 @@
 console.log('marie loadeds...')
+
+marie={}
+
+marie.ui=function(div){
+    marie.div=div||document.getElementById('workSpace')
+    if(marie.div){
+        let h = `<h3>hello world</h3>
+        <p>
+        how are you doing?
+        today is ${Date()}.
+        </p>
+        <p id="some Math"><input id="x">+<input id="y">=<spanid="result"></span>
+        </p>`
+        marie.div.innerHTML=h
+        x.onkeyup=function(){console.log('stop pressing me at '+Date())}
+
+    }
+}
+
+window.onload=function(){
+    if(document.getElementById('workSpace')){
+        marie.ui(document.getElementById('workSpace'))        
+    }
+
+    
+}
