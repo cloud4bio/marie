517b199ccdbfba757de6726d765fe527f2f9d426
Marie <marieb2@nih.gov>
2019-06-18 09:57:22 -0400

    can select operation
    but can't yet switch between them afterwards

--- marie.js
+++ marie.js
@@ -7,42 +7,91 @@ marie.ui=function(div){
 
     if(marie.div){
 
-        //Create buttons for user to pick operation
-        var addButton = document.createElement("Button")    //addition button
-        addButton.innerHTML = "addition"
-        document.body.appendChild(addButton);    
-
-        var subtractButton = document.createElement("Button")   //subtraction button
-        subtractButton.innerHTML = "subtraction"
-        document.body.appendChild(subtractButton);   
-
-        var multiplyButton = document.createElement("Button")   //multiplication button
-        multiplyButton.innerHTML = "multiplication"
-        document.body.appendChild(multiplyButton);     
-
-        var divideButton = document.createElement("Button")     //division button
-        divideButton.innerHTML = "division"
-        document.body.appendChild(divideButton);    
-
         //Header and text 
+        //Create buttons for user to pick operation
         let h = `<h3>hello world</h3>   
        <!-- <p>
         how are you doing?
         today is ${Date()}. 
         </p> -->
         <p> Welcome to the calculator! Choose an operation: </p>
-        <p id="some Math"><input id="x">+<input id="y">=<span id="result"></span>
+
+        <Button id = "addition"> + </Button>
+        <Button id = "subtraction"> - </Button>
+        <Button id = "multiplication"> * </Button>
+        <Button id = "division"> / </Button>
+
+        <!-- <p id="addition"><input id="x">+<input id="y">=<span id="result"></span>
         </p>
         <p id="subtraction"><input id="a">-<input id="b">=<span id="subAnswer"></span>
-        </p>`
+        </p> --> `
         marie.div.innerHTML=h
+        //marie.div.innerHTML += '<p>test</p>'
+
+        document.getElementById("addition").onclick = function(){add()}
+        document.getElementById("subtraction").onclick = function(){subtract()}
+        document.getElementById("division").onclick = function(){divide()}
+        document.getElementById("multiplication").onclick = function(){multiply()}
+        
+        
+        function add(){
+             let inputs = `<p id="addText"><input id="x"> + <input id="y"> = <span id="result"></span>
+        </p>`
+            marie.div.innerHTML += inputs
+            x.onkeyup=y.onkeyup=function(){
+                result.innerText=parseFloat(x.value)+parseFloat(y.value)
+            }
+        }
+
+        function subtract(){
+             let inputs = `<p id="subtractText"><input id="x"> - <input id="y"> = <span id="result"></span>
+        </p>`
+            marie.div.innerHTML += inputs
+            x.onkeyup=y.onkeyup=function(){
+                result.innerText=parseFloat(x.value)-parseFloat(y.value)
+            }
+        }
+
+        function multiply(){
+             let inputs = `<p id="multiplyText"><input id="x"> * <input id="y"> = <span id="result"></span>
+        </p>`
+            marie.div.innerHTML += inputs
+            x.onkeyup=y.onkeyup=function(){
+                result.innerText=parseFloat(x.value)*parseFloat(y.value)
+            }
+        }
+
+        function divide(){
+             let inputs = `<p id="divideText"><input id="x"> / <input id="y"> = <span id="result"></span>
+        </p>`
+            marie.div.innerHTML += inputs
+            x.onkeyup=y.onkeyup=function(){
+                result.innerText=parseFloat(x.value)/parseFloat(y.value)
+            }
+        }
+
+        //Create buttons for user to pick operation
+        /*var addButton = document.createElement("Button")    //addition button
+        addButton.innerHTML = "addition"
+        document.body.appendChild(addButton)    
+        //addButton.onClick = function(){console.log("test")}
+        //addButton.onClick = add()
+        
+        var subtractButton = document.createElement("Button")   //subtraction button
+        subtractButton.innerHTML = "subtraction"
+        document.body.appendChild(subtractButton)  
+
+        var multiplyButton = document.createElement("Button")   //multiplication button
+        multiplyButton.innerHTML = "multiplication"
+        document.body.appendChild(multiplyButton)   
+
+        var divideButton = document.createElement("Button")     //division button
+        divideButton.innerHTML = "division"
+        document.body.appendChild(divideButton)   
         x.onkeyup=y.onkeyup=function(){
             result.innerText=parseFloat(x.value)+parseFloat(y.value)
         }
-        a.onkeyup=b.onkeyup=function(){
-            subAnswer.innerText=parseFloat(a.value)-parseFloat(b.value)
-        }
-      //let test =
+        */
         
     }
 }
