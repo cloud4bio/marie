97a2948ea0477ca71e8b1e930c481896d1152282
mariebrodsky <brodskymarie@gmail.com>
2019-06-19 13:34:48 -0400

    accessing iris data

--- marie.js
+++ marie.js
@@ -1,5 +1,6 @@
 console.log('marie loadeds...')
 
+
 marie={}
 
 
@@ -7,12 +8,31 @@ marie.ui=function(div){
     marie.div=div||document.getElementById('workSpace')
 
     if(marie.div){
-        
+
         //FLOWER SORTING PROJECT
+
+        async function getData() {
+          const irisOriginalFile = await fetch('https://episphere.github.io/ai/data/iris.json');  
+          const irisFile = await irisOriginalFile.json();  
+          const irisCleaned = irisFile.map(flower => ({
+            sepLen: flower.sepalLength,
+            sepWid: flower.sepalWidth, 
+            petLen: flower.petalLength,
+            petWid: flower.petalWidth,
+            correctSpecies: flower.species,}))
+          return irisCleaned
+        }
+          
+          irisCleaned = getData()
+       
+
+        //var irisData = JSON.parse(irisCleaned);
+        //alert(irisData[0].sepalLength);
+
         //Header and text
         let h = `<h3>Flower Sorting Project</h3>`
        
-       
+
         marie.div.innerHTML=h
         
     }
