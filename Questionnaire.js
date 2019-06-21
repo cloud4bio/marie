71f8c6d5516f8abe2a83f407358171b8667c91d4
mariebrodsky <brodskymarie@gmail.com>
2019-06-21 09:30:36 -0400

    multiple questions in JSON

--- Questionnaire.js
+++ Questionnaire.js
@@ -13,6 +13,8 @@ survey.ui=function(div){
         let h = `<h3>Risk Prediction Questionnaire</h3>`
         survey.div.innerHTML=h
 
+
+        /*Reading from JSON test
         myObj = {Age: 40, FamilyHistory: 'Y', PreviousDiagnosis: 'N', Children: 'Y', Mammogram: 'N'};
         myJSON = JSON.stringify(myObj);
         localStorage.setItem("testJSON", myJSON);   //tentatively saving JSON to local storage
@@ -34,10 +36,40 @@ survey.ui=function(div){
 
         console.log(finalStr)
         h += finalStr
-        survey.div.innerHTML=h
-    }
+        survey.div.innerHTML=h*/
 
-}
+        /*Sample JSON File Format
+        [
+            {
+               “@id”:<some id, we can create it automatically as a uuid>,
+                “question”:<some question>,
+                “type”:<multiple choice, numeric, free text, etc>,
+                “options”:[list of options for multiple choice],
+                “onDone”:function listening to the event of an answer, this is open ended, from skip logic to engaging model prediction,
+                “help”: “some help to understand the question:
+            }
+            ... [other questions]
+        ]*/
+
+        questionsObj = 
+        [
+            {QuestionID: 314, Question: 'Family History?', Type: 'M', Options: ['Yes', 'No'], onDone: 'none', help: 'Clarifying Information'},
+            {QuestionID: 315, Question: 'Children?', Type: 'M', Options: ['Yes', 'No'], onDone: 'none', help: 'Clarifying Information'}
+        ]
+        questionsJSON = JSON.stringify(questionsObj);
+        localStorage.setItem("questionsJSON", questionsJSON);   //tentatively saving JSON to local storage
+        console.log(questionsJSON)
+
+        // Retrieving data:
+        readText = localStorage.getItem("questionsJSON");
+        retrievedObj = JSON.parse(readText);
+        var i
+        for(i = 0; i < retrievedObj.length; i++){
+            console.log(retrievedObj[i].Question)
+        }
+    }   //close [if(survey.div)]
+
+} //close[survey.ui=function(div)]
 
 //On page startup
 window.onload=function(){
