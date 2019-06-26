b07dde460cd24f81260f8e9a4d0c4ceb2e6ed06a
mariebrodsky <brodskymarie@gmail.com>
2019-06-26 11:44:30 -0400

    one question at a time, response logged

--- newQuestionnaire.js
+++ newQuestionnaire.js
@@ -11,15 +11,15 @@ survey.ui = function(div) {
         let h = `<h3>Risk Prediction Questionnaire</h3>`
         let more = `<p>Learn more at <a href = "https://github.com/episphere/ai/wiki/cancer-risk-modeling" target = "_blank"> the GitHub Wiki</a>.</p>`
         h += more
-        survey.div.innerHTML = h
+        survey.div.innerHTML = h;
 
 (async function(){
         async function readByIDs(url){
             //q = readJSON()
-            url = url||'https://cloud4bio.github.io/marie/questions.json'
+            url = url||'http://localhost:8000/marie/questions.json'     //update to cloud4bio page
             q = await (await fetch(url)).json()
             qq={}
-            q.forEach(qi=>{qq[qi['@id']]=qi})
+            q.forEach(qi=>{qq[qi['ID']]=qi})
             return qq       
         }
 
@@ -33,20 +33,25 @@ survey.ui = function(div) {
         var resultDict = {}
         
         //obtain starting question
-        startID = 314
+        startID = 316
         currItem = await getQuest(startID)
-
+        
+        //WHILE LOOP HERE
         //access attributes of question
         var currQuestion = currItem.Question
         var type = currItem.Type
         var currID = currItem.ID
         var guidance = currItem.help
+        var nextQuest = currItem.onDone
 
         var newDiv = document.createElement('div');
         document.body.appendChild(newDiv);
 
         var quest = document.createTextNode(currQuestion);
         document.body.appendChild(quest);
+
+        console.log(type)
+        createQuest(type);
 
         function createQuest(type){     //want to input ID too?
             switch(type){
@@ -73,6 +78,7 @@ survey.ui = function(div) {
                     newBubble.setAttribute("type", "radio")
                     document.body.appendChild(newBubble)
 
+                    newBubble.onclick = function(){moveOn(nextQuest);console.log('clicked')}
                     //store result somehow (not sure since multiple choice options are unlimited)
                 }
        }
@@ -85,6 +91,7 @@ survey.ui = function(div) {
          inputBox.onkeyup=function(){
             let numReply = document.getElementById('userInput').value
             resultDict[currID] = numReply; console.log(resultDict)
+            moveOn(nextQuest)
          }
       }
 
@@ -96,7 +103,12 @@ survey.ui = function(div) {
           checkBox.onclick=function(){
               let boolean = document.getElementById('checkInput').checked
               resultDict[currID] = boolean; console.log(resultDict)
+              moveOn(nextQuest)
           }
+      }
+
+      function moveOn(next){
+//
       }
 
 })()
