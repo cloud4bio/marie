826083092890bf41cee9c51936d8bc1b4f52575c
mariebrodsky <brodskymarie@gmail.com>
2019-07-01 10:36:14 -0400

    split data between training and testing sets

--- BreastCancerModel.js
+++ BreastCancerModel.js
@@ -12,9 +12,61 @@ riskUI.ui=function(div){
         let description = '<p>Please wait for the model to load.</p>'
         riskUI.div.innerHTML=h+description
 
+        const outcomes = ['developed_cancer','no_cancer']
+        const num_outcomes = outcomes.length
+        
+        const cancer_data = [] //ACCESS DATA FILE
+        //need to convert to readable format,
+        //each study subject in their own array object
 
-    }
-}
+        //testSplit represents the fraction of data used for testing (e.g. 0.2)
+        function getCancerData(testSplit){
+            return tf.tidy(() => {
+                const dataByClass = [];
+                const targetsByClass = [];
+                for(let i = 0; i < outcomes.length; ++i){
+                    dataByClass.push([]);
+                    targetsByClass.push([]);
+                }
+
+                //sort data by class (whether or not cancer developed)
+                for(const example of cancer_data){
+                    const target = example[example.length-1];
+                    const data = example.slice(0,example.length-1);
+                    dataByClass[target].push(data);
+                    targetsByClass[target].push(target);
+                }
+                console.log(dataByClass);
+                console.log(targetsByClass);
+
+                //tensors to hold training and testing datasets
+                const xTrains = [];
+                const yTrains = [];
+                const xTests = [];
+                const yTests = [];
+                for(let i =0; i < outcomes.length; ++i){
+                    const[xTrain, yTrain, xTest, yTest] = 
+                        //convert to tensors to be used for training
+                        convertToTensors(dataByClass[i], targetsByClass[i], testSplit);
+                    xTrains.push(xTrain);
+                    yTrains.push(yTrain);
+                    xTests.push(xTest);
+                    yTests.push(yTest);
+                }
+
+                //concatonate testing and training data into 1D tensors
+                const concatAxis = 0;
+                const test1 = xTrains;
+                const test2 = tf.concat(xTrains, concatAxis)
+                console.log(test1)
+                console.log(test2)
+                return [
+                    tf.concat(xTrains,concatAxis), tf.concat(yTrains, concatAxis),
+                    tf.concat(xTests,concatAxis), tf.concat(yTests, concatAxis)]
+            })
+        }   //end of getCancerData function
+    }   //end of if(riskUI.div)
+}   //end of riskUI.ui=function(div)
 
 //On page startup
 window.onload=function(){
